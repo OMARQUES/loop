a7816f6619d3a73a725c452dac9d133387b86441
Omarques <otavio.marques20@hotmail.com>
2024-02-07 13:58:00 -0300

    Mensagens tela de login

--- app/components/Menus/DesktopNav.tsx
+++ app/components/Menus/DesktopNav.tsx
@@ -29,13 +29,15 @@ const DesktopNav = () => {
           <>
             {" "}
             <Link
-              className="border-[1px] hover:bg-green-700 hover:text-white mx-4 min-w-[168px] w-full text-center py-[10px] px-8 text-sm font-medium  border-solid rounded-[24px] border-green-700"
+              className="border-[1px] mx-4 min-w-[168px] w-full text-center py-[10px] px-8 text-sm font-medium  border-solid rounded-[24px] border-green-700
+              hover:bg-green-700 hover:text-white transition ease-in-out delay-150 hover:scale-110"
               href="/login"
             >
               <p>Entrar</p>
             </Link>
             <Link
-              className="rounded-[24px] hover:bg-white hover:border-2 hover:text-green-700 hover:border-green-700 text-white font-medium text-sm shadow-button py-[10px] mx-4 min-w-[168px] w-full text-center bg-green-700 "
+              className="rounded-[24px] text-white font-medium text-sm shadow-button py-[10px] mx-4 min-w-[168px] w-full text-center bg-green-700
+              hover:bg-green-800  hover:border-green-700 transition ease-in-out delay-150 hover:scale-110"
               href="/register"
             >
               <p>Cadastrar</p>
--- app/components/Menus/MobileNav.tsx
+++ app/components/Menus/MobileNav.tsx
@@ -12,18 +12,20 @@ const MobileNav = () => {
   const { data: session } = useSession();
 
   return (
-    <nav className="flex max-w-full mx-auto sm:p-6 justify-between items-center lg:hidden p-2 cursor-pointer bg-slate-200">
-      <h1 className=" font-semibold tracking-wider sm:text-lg z-[100]">
-        <Link href="/home">
-          <MdDeliveryDining className="w-12 h-12 fill-current text-red-600" />
-          Delivo
-        </Link>
-      </h1>
+    <nav className="flex max-w-full mx-auto sm:p-2 justify-between items-center lg:hidden p-1 cursor-pointer bg-slate-200">
+      <div className="flex items-center transition ease-in-out delay-150 hover:scale-110">
+        <h1 className=" font-semibold tracking-wider sm:text-lg z-[100]">
+          <Link href="/home">
+            <MdDeliveryDining className="w-12 h-12 fill-current text-red-600" />
+            Delivo
+          </Link>
+        </h1>
+      </div>
       <div className="z-[100] relative">
         <Hamburger size={25} label="Show menu" toggled={isOpen} toggle={setOpen} />
       </div>
       {isOpen && (
-        <div className="flex absolute w-screen pt-[25%] bg-white z-50 inset-0 h-screen flex-col items-center cursor-pointer">
+        <div className="flex absolute w-screen pt-32 bg-slate-200 z-50 inset-0 h-screen flex-col items-center cursor-pointer">
           {session ? (
             <>
               <p className="my-4 text-black  ">Signed in as {session.user?.email}</p>
@@ -33,26 +35,25 @@ const MobileNav = () => {
             </>
           ) : (
             <div>
-              <p className="border-[1px] w-full my-4 text-center py-[10px] text-sm font-medium  border-solid rounded-[24px] px-6 border-green-700">
-                <Link
-                  href="/login"
-                  onClick={() => {
-                    setOpen(false);
-                  }}
-                >
-                  Log in
-                </Link>
-              </p>
-              <p className="rounded-[24px] my-4 text-white font-medium text-sm shadow-button py-[10px] px-6 w-full text-center bg-green-700 ">
-                <Link
-                  href="/register"
-                  onClick={() => {
-                    setOpen(false);
-                  }}
-                >
-                  Register
-                </Link>
-              </p>
+              <Link
+                className="flex justify-center border w-60 my-5 text-center py-4 text-sm font-medium border-solid rounded-full px-6 border-green-700"
+                href="/login"
+                onClick={() => {
+                  setOpen(false);
+                }}
+              >
+                <p>Log in</p>
+              </Link>
+
+              <Link
+                className="flex justify-center border w-60 my-5 text-center py-4 text-sm font-medium border-solid rounded-full px-6 bg-green-700 text-white border-green-700"
+                href="/register"
+                onClick={() => {
+                  setOpen(false);
+                }}
+              >
+                <p>Register</p>
+              </Link>
             </div>
           )}
         </div>
